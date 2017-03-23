26d1db07a17273e133677d71ad3e4a5ae9f3163b
E Jun <jason_e@syscom.com.cn>
2017-03-23 10:20:38 +0800

    add CRUD model

--- src/main/java/com/phalaenopsis/service/UserServiceImpl.java
+++ src/main/java/com/phalaenopsis/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.phalaenopsis.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +22,30 @@ public class UserServiceImpl implements UserService{
 
 	public User findBySso(String sso) {
 		return userRepository.findBySsoId(sso);
+	}
+	
+	public void saveUser(User user) {
+		userRepository.save(user);
+	}
+
+	public void updateUser(User user){
+		saveUser(user);
+	}
+
+	public void deleteUserById(Long id){
+		userRepository.delete(id);
+	}
+
+	public void deleteAllUsers(){
+		userRepository.deleteAll();
+	}
+
+	public List<User> findAllUsers(){
+		return userRepository.findAll();
+	}
+
+	public boolean isUserExist(User user) {
+		return findBySso(user.getSsoId()) != null;
 	}
 
 }
