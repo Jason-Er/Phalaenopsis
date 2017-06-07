b2654c5121b3ccafdc8667f8f91b6da480644e9b
E Jun <jason_e@syscom.com.cn>
2017-06-07 11:39:21 +0800

    return custom auth user name; the authentication is set in JWTAuthenticationFilter line 23

--- src/main/java/com/phalaenopsis/util/AuthUtil.java
+++ src/main/java/com/phalaenopsis/util/AuthUtil.java
@@ -5,14 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class AuthUtil {
 	public static String getAuthUserName() {
-		String userName = null;
-		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-		if (principal instanceof UserDetails) {
-			userName = ((UserDetails) principal).getUsername();
-		} else {
-			userName = principal.toString();
-		}
-		return userName;
+		return SecurityContextHolder.getContext().getAuthentication().getName();
 	}
 }
